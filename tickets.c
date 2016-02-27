3ae52813cffa48b329eab32c8c7ac49b89a9a568
Dylan Johnson <spfyyy@gmail.com>
2016-02-27 03:35:52 -0800

    thread code started so people know how it works. temp code.

--- tickets.c
+++ tickets.c
@@ -6,13 +6,16 @@
 #define TRUE        1
 #define FALSE       0
 
+/* a seat's id should say who sold it */
 typedef struct seat
 {
 	char * id;
 	int taken;
 } seat;
 
+/* the seats are critical, so a thread needs the lock to read/write */
 seat seats[100];
+pthread_mutex_t seat_lock;
 
 /* create an array of empty seats */
 void init_seats()
@@ -36,10 +39,49 @@ void show_seats()
 	}
 }
 
+/* check if a seat is empty */
+int is_empty(seat s) {
+	return s.taken ? FALSE : TRUE;
+}
+
+/* find the first empty seat */
+int find_empty_seat()
+{
+	int i;
+	for (i = 0; i < 100; ++i) {
+		seat s = seats[i];
+		if (is_empty(s)) return i;
+	}
+	return -1;
+}
+
+/* mark a specific seat as taken, using its index */
+void take_spot(int i)
+{
+	if (i >= 0 && i < 100) {
+		seat *s = &seats[i];
+		s->taken = TRUE;
+		s->id = "GONE\0";
+	}
+}
+
 /* generate a random number between low and high (inclusive) */
 int random_num(int low, int high)
 {
 	return rand() % (high + 1 - low) + low;
+}
+
+/* low price selling thread */
+void *low_price_seller(void *arg)
+{
+	while (TRUE) {
+		pthread_mutex_lock(&seat_lock);
+		int spot = find_empty_seat();
+		take_spot(spot);
+		pthread_mutex_unlock(&seat_lock);
+		if (spot < 0) pthread_exit(NULL);
+		show_seats();
+	}
 }
 
 /* main thread */
@@ -48,8 +90,11 @@ int main(int argc, char **argv)
 	/* program initialization */
 	init_seats();
 	srand(time(0));
+	pthread_mutex_init(&seat_lock, NULL);
 	
 	/* start creating threads here */
+	pthread_t threads[10];
+	pthread_create(&threads[0], NULL, low_price_seller, NULL);
 	
 	pthread_exit(NULL);
 }
